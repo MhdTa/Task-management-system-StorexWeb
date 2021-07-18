08c5b28a3e91d11d555a7a83ddf419d308b64920
Mohamed Taha <49076857+MhdTa@users.noreply.github.com>
2021-07-18 13:27:05 +0300

    Add files via upload

---
+++ database/migrations/2021_07_15_125942_create_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('check_list_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->string('status')->default('todo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
